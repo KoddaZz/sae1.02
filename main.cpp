389c9000d3e22311535e11b5c00f1f7e08f9da78
KoddaZz <jules.fuselier0401@gmail.com>
2024-10-20 19:13:26 +0200

    update

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main() {
         "Christopher", "Hannah", "Daniel", "Victoria", "Matthew", "Kayla", "Anthony", "Alexis", "Donald", "Rachel",
         "Mark", "Madison", "Paul", "Natalie", "Steven", "Alyssa", "Andrew", "Grace", "Joshua", "Olivia",
         "Brian", "Sophia", "Kevin", "Chloe", "George", "Isabella", "Edward", "Emma", "Ronald", "Ava",
-        "Timothy", "Mia", "Kenneth", "Abigail", "Jason", "Emily", "Eric", "Ella", "Jeffrey", "Madeline", // "Emily" en double
+        "Timothy", "Mia", "Kenneth", "Abigail", "Jason", "Emily", "Eric", "Ella", "Jeffrey", "Madeline",
         "Ryan", "Addison", "Jacob", "Hailey", "Nicholas", "Charlotte", "Stephen", "Harper", "Frank", "Lily",
         "Patrick", "Brooklyn", "Scott", "Layla", "Justin", "Zoe", "Larry", "Penelope", "Brandon", "Scarlett",
         "Benjamin", "Stella", "Samuel", "Nora", "Gregory", "Eleanor", "Alexander", "Violet", "Henry", "Ellie",
@@ -36,16 +36,13 @@ int main() {
         "Henry", "Ruby", "Louis", "Cora", "Joe", "Emilia", "Howard", "Peyton", "Billy", "Rylee"
     };
 
-    map<string, int> candidatsNationaux;
-    candidatsNationaux["Trump"] = 0;
-    candidatsNationaux["Bieden"] = 0;
-    candidatsNationaux["Clinton"] = 0;
+
     cout << "Bienvenue sur une simulation d'un vote !" << endl;
     unsigned nombreDeVotants = 0; // Initialisation de nombreDeVotants
     unsigned Compteur = 0;
-    unsigned nombreDeVoteBlanc = 0;
     unsigned nombreRegion = 0;
     unsigned nombreDeleguesRegion = 0;
+    unsigned nombreCandidatsNationaux;
     unsigned nomAuHasard = 0;
     string saisiNomCandidat = " ";
     cout << "Combien d'Etats / region voulez vous simuler ? ";
@@ -53,6 +50,7 @@ int main() {
     cout << "Combien de candidat Delegues voulez vous par région ? ";
     cin >> nombreDeleguesRegion;
     unsigned compteur2 = 0;
+    unsigned nombreVoteBlancNational;
 
     map<int, string> dicoDeleguesParRegions;
 
@@ -101,11 +99,56 @@ int main() {
         for (unsigned int k = 0; k < 3 && k < liste_candidats.size(); k++) {
             cout << k+1 << ". " << liste_candidats[k].first << " avec " << liste_candidats[k].second << " vote(s)." << endl;
         }
-        cout << "Nombre de votes blancs : " << nombreVotesBlancParRegions[i] << endl << " " << endl << " =========================================== " << endl << "      NOUVEAU VOTE / NOUVELLE REGION      " << endl << " =========================================== " << endl << " " << endl;
+        cout << "Nombre de votes blancs : " << nombreVotesBlancParRegions[i] << endl << " " << endl << " ===================== [ REGIONAL ] ===================== " << endl << "      NOUVEAU VOTE / NOUVELLE REGION      " << endl << " ===================== [ REGIONAL ] ===================== " << endl << " " << endl;
 
         dicoDeleguesParRegions[i] = liste_candidats[0].first;
     }
 
+    map<string, int> candidatsNationaux;
+    // Générer les délégués de manière unique
+    for (unsigned j = 0; j < nombreCandidatsNationaux; j++) {
+        nomAuHasard = rand() % prenoms.size();
+        auto prenom = prenoms[nomAuHasard];
+        if (candidatsNationaux.find(prenom) == candidatsNationaux.end()) {
+            candidatsNationaux[prenom] = 0;
+        }
+    }
+
+    cout << " " << endl << " ===================== [ NATIONAL ] ===================== " << endl << "      VOTE CANDIDAT NATIONAL     " << endl << " ===================== [ NATIONAL ] ===================== " << endl << " " << endl;
+
+    while (compteur2 < nombreRegion) {
+        // ANNONCE DES CANDIDATS NATIONAUX
+
+        cout << "La liste des candidats est : " << endl;
+        for (const auto& votant : candidatsNationaux) {
+            int idx = 1;  // Correction d'affichage manquée.
+            for (const auto& pair : votant.second) {
+                cout << idx << ". " << pair.first << endl;
+                idx++;
+            }
+        }
+        cout << "Entrez le nom du candidat : ";
+        cin >> saisiNomCandidat;
+        cout << endl;
+        compteur2++;
+
+        if (candidatsNationaux.find(saisiNomCandidat) != candidatsNationaux.end()) {
+            candidatsNationaux[saisiNomCandidat] += 1;
+        } else {
+            ++nombreCandidatsNationaux;
+        }
+    }
+    vector<pair<string, int>> liste_candidats(candidatsNationaux.begin(), candidatsNationaux.end());
+
+    sort(liste_candidats.begin(), liste_candidats.end(), [](pair<string, int> a, pair<string, int> b) {
+        return a.second > b.second;
+    });
+    // Afficher le podium
+    cout << "Podium des candidats : " << endl;
+    for (unsigned int i = 0; i < 3 && i < liste_candidats.size(); i++) {
+        cout << i+1 << ". " << liste_candidats[i].first << " avec " << liste_candidats[i].second << " vote(s)." << endl;
+    }
+    cout << "Nombre de votes blancs : " << nombreVoteBlancNational << endl;
 
     return 0;
 }
